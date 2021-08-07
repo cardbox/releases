e09d6259568158d6fd3f004814e53b2a47afd4f4
Kirill Mironov <vetrokm@gmail.com>
2021-08-07 15:55:29 +0300

    Use mkcert and update readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# releases
+# cardbox releases
+
+Deploy workflows
+
+## Setup local instance
+
+1. Install `docker` ([install for mac](https://docs.docker.com/docker-for-mac/install/)) and `docker-compose` ([install](https://docs.docker.com/compose/install/))
+1. Clone this repository to `cardbox-releases` directory (it helps to identify images in `docker ps` list)
+1. `cd cardbox-releases`
+1. Copy `.env.sample` to `.env`. Modify content of `.env. if required
+1. Start services with `docker-compose up -d`. Stop it with `docker-compose down`
+
+## Instructions
+<details><summary>Install certificate</summary>
+Install mkcert:
+
+# macOS
+```
+brew install mkcert
+brew install nss # if you use Firefox
+```
+# linux
+Get latest mkcert release from ([mkcert releases](https://github.com/FiloSottile/mkcert/releases))
+
+Run `mkcert -install`
+
+Run `scripts/create-cert/sh`
+</details>
--- scripts/create-cert.sh
+++ scripts/create-cert.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-openssl req -x509 -out ./tls/cardbox.crt -keyout ./tls/cardbox.pem \
-  -newkey rsa:2048 -nodes -sha256 \
-  -subj '/CN=cardbox.local' -extensions EXT -config <( \
-   printf "[dn]\nCN=cardbox.local\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:cardbox.local\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
-
-openssl dhparam -out ./tls/dhparam.pem 2048#!/bin/bash
-
-openssl req -x509 -out ./tls/cardbox.crt -keyout ./tls/cardbox.pem \
-  -newkey rsa:2048 -nodes -sha256 \
-  -subj '/CN=cardbox.local' -extensions EXT -config <( \
-   printf "[dn]\nCN=cardbox.local\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:cardbox.local\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
-
-openssl dhparam -out ./tls/dhparam.pem 2048
+mkcert -key-file tls/accesso-local.pem -cert-file tls/accesso-local.cert accesso.localhost "*.accesso.localhost"
